1ac807b93dc4b333f45d3711e0cb7e615696e30d
khojah <khojah@student.chalmers.se>
2020-12-02 17:42:11 +0100

    created the DataGenerator class with iter and next functions

--- tasks/data_generator.py
+++ tasks/data_generator.py
@@ -28,4 +28,29 @@ following code should run:
 """
 
 class DataGenerator:
-    pass
+    def __init__(self, files):
+        #the constructor of DataGenerator class holds the "filtered" files as one of its attributes
+        new_files = [file for file in files if open(file).readline() == file]
+        self.files = list(new_files)
+
+    def __iter__(self):
+        #the _iter_ function initializes the index of the iterator i.e. 0
+        self.i = 0
+        return self
+
+    def __next__(self):
+        #gets the file name in the next index
+        next_file = self.files[self.i]
+        #increment the index by 1
+        self.i += 1
+        return next_file
+
+
+# #Uncomment to test
+# files = ["file1.txt", "file2.txt", "ignore1.txt", "file3.txt", "ignore2.txt", "file4.txt"]
+# my_generator =  DataGenerator(files)
+# for data in my_generator:
+#     print(data)
+#     if "3" in data:
+#         break
+# print(next(my_generator))
