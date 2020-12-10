8d6d0b4c13782ebcca61c84de6ca068b9c009592
patriciagrau <75249084+patriciagrau@users.noreply.github.com>
2020-12-10 18:27:08 +0100

    Update checker.py

--- tasks/checker.py
+++ tasks/checker.py
@@ -5,7 +5,26 @@ def what_is_this(a):
     """Returns the type of variable given.
     
     Working from the terminal $ it will always return str. 
-    Please import it in Python with from checker import what_is_this."""
+    Please import it in Python with from checker import what_is_this.
+    
+    given a variable it should, 
+    
+    if its a function: 
+        return "function"
+    
+    if its a class object:
+        return "class":
+    else:
+        return the type of the varible
+    examples:
+    
+    >> what_is_this(1) -> int
+    
+    >> def f():
+    >>     pass
+    >> what_is_this(f) -> "function"
+    
+    """
     
     return type(a)
 
