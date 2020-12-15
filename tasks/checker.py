ffd3d35007755e8cb0a1e5defb940655dbeff2e6
patriciagrau <75249084+patriciagrau@users.noreply.github.com>
2020-12-15 14:00:55 +0100

    Update checker.py

--- tasks/checker.py
+++ tasks/checker.py
@@ -1,11 +1,6 @@
-import sys
-
 
 def what_is_this(a):
     """Returns the type of variable given.
-    
-    Working from the terminal $ it will always return str. 
-    Please import it in Python with from checker import what_is_this.
     
     given a variable it should, 
     
@@ -27,9 +22,3 @@ def what_is_this(a):
     """
     
     return type(a)
-
-def main():
-	print(what_is_this(sys.argv[1]))
-
-if __name__ == '__main__':
-	main()
