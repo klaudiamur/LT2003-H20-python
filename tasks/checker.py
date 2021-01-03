d66e6238c1ce78a5f7fdacffe0b76a0251dffb7c
patriciagrau <gusgraupa@students.gu.se>
2021-01-03 11:46:37 +0100

    Change checker.py to work with class type

--- tasks/checker.py
+++ tasks/checker.py
@@ -1,16 +1,24 @@
-import sys
-
-
 def what_is_this(a):
-    """Returns the type of variable given.
+	"""Returns the type of variable given.
     
-    Working from the terminal $ it will always return str. 
-    Please import it in Python with from checker import what_is_this."""
+    given a variable it should, 
     
-    return type(a)
-
-def main():
-	print(what_is_this(sys.argv[1]))
-
-if __name__ == '__main__':
-	main()
+    if its a function: 
+        return "function"
+    
+    if its a class object:
+        return "class":
+    else:
+        return the type of the varible
+    examples:
+    
+    >> what_is_this(1) -> int
+    
+    >> def f():
+    >>     pass
+    >> what_is_this(f) -> "function"
+	"""
+	if type(a) == type:
+		print('<class \'class\'>')
+	else:
+		return(type(a))
